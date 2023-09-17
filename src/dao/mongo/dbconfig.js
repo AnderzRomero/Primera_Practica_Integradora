2fdc7a8055001604a978e443526df7172a3b022c
Anderson Romero <andersonromerorico@gmail.com>
2023-09-17 07:35:42 -0500

    se crea variables de entorno para conexion a la Base de datos Mongos

--- src/dao/mongo/dbconfig.js
+++ src/dao/mongo/dbconfig.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const connection = "mongodb+srv://coder:ander1234@cluster0.9vhlkqi.mongodb.net/ecommerce?retryWrites=true&w=majority";
+dotenv.config();
 
-await mongoose.connect(connection, { serverSelectionTimeoutMS: 5000, });
+const URL = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.9vhlkqi.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
-console.log("Base de datos conectada con exito");
+await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000, })
+    .then(() => console.log("Base de datos conectada con exito"))
+    .catch(e => console.log(e));
+
+
+
+
 
 
 
